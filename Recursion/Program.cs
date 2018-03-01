feabfa8b6b9c09ae8d9d826af44f94383de71f42
SergeiAnufriev <seregioas@gmail.com>
2018-03-01 10:56:07 +0300

    Initial commit

---
+++ Recursion/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace Recursion
+{
+    class Program
+    {
+        // Рекурсивный метод
+        /*static void natural(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+
+        static void PrintNDigits(int n)
+        {
+            if (n == 0)
+            {
+                Console.WriteLine("Базовый cлучай n = 1");
+                return;
+            }
+
+            Console.WriteLine("Рекурсивный вызов с n=" + n);
+            PrintNDigits(n - 1);
+            Console.WriteLine(n);
+        }
+
+       static void TwoNumbers(int n1, int n2)
+       {
+           if (n1 == n2)
+           {
+               Console.WriteLine(n1);
+               return;
+           }
+
+           if (n1 > n2)
+           {
+               Console.WriteLine(n1);
+               TwoNumbers(n1 - 1, n2);
+
+           }
+           else
+           {
+               Console.WriteLine(n1);
+               TwoNumbers(n1 + 1, n2);
+           }
+       }
+
+       static int Akkerman(int m, int n)
+       {
+           if (m == 0)
+           {
+               return   n + 1;
+
+           }
+
+           if (m > 0 && n == 0)
+           {
+               return  Akkerman(m - 1, 1);
+           }
+
+           if (m > 0 && n > 0)
+           {
+               return  Akkerman(m - 1, Akkerman(m, n - 1));
+           }
+
+           return 0;
+       }*/
+
+        static void Power(float n)
+        {
+            if (n == 2)
+            {
+                Console.WriteLine("Yes");
+                return;
+            }
+
+            if (n < 2)
+            {
+                Console.WriteLine("No");
+                return;
+            }
+            else
+            {
+                Power(n / 2);
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+
+            Console.Write("Введите число: ");
+
+            // int m = int.Parse(Console.ReadLine());
+            float n = int.Parse(Console.ReadLine());
+            Power(n);
+
+            //int x = Akkerman(m, n);
+
+            //Console.WriteLine(x);
+
+            Console.ReadLine();
+
+        }
+    }
+}
